a4cc6100962cca41757657e7371aa44837326683
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-26 17:09:03 +0530

    addedllllllll

---
+++ src/com/sample/Sample.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class Sample {
+
+	public static void main(String[] args){
+	String str="ADHITHYA";
+	System.out.println(str.toLowerCase());
+		
+			
+				
+		}
+		
+		
+		
+
+	}
+
+}
